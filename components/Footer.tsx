2a2a7f823dbbd473e0201fd26761f637b36741ba
OC <chandankarom07@gmail.com>
2025-12-23 22:20:33 +0530

    updated navbar and footer with logo and added logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,7 @@ import Wrapper from "./Wrapper";
 import Link from "next/link";
 import { Instagram } from "lucide-react";
 import { BsTwitterX, BsLinkedin } from "react-icons/bs";
+import Image from "next/image";
 
 const Footer = () => {
   return (
@@ -9,7 +10,13 @@ const Footer = () => {
       <footer className="mt-7.5">
         <div className="border-b border-border pb-4">
           <div className="flex justify-between items-center">
-            <img src="/assets/vector.svg" alt="Logo" className="h-6" />
+            <Image
+              src="/assets/Logo.png"
+              width={200}
+              height={25}
+              alt="Logo"
+              className="h-6"
+            />
             <div className="flex gap-3">
               <Instagram className="h-5 w-5" />
               <BsTwitterX className="h-5 w-5" />
@@ -20,8 +27,18 @@ const Footer = () => {
         <div className="flex justify-between items-center mt-2">
           <p className="text-xs">© 2025 FlowerShop. All rights reserved.</p>
           <div className="flex gap-5">
-            <Link href="/terms" className="text-xs font-medium tracking-wider hover:underline">Terms of Use</Link>
-            <Link href="/privacy" className="text-xs font-medium tracking-wider hover:underline">Privacy Policy</Link>
+            <Link
+              href="/terms"
+              className="text-xs font-medium tracking-wider hover:underline"
+            >
+              Terms of Use
+            </Link>
+            <Link
+              href="/privacy"
+              className="text-xs font-medium tracking-wider hover:underline"
+            >
+              Privacy Policy
+            </Link>
           </div>
         </div>
       </footer>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,40 @@
 import Wrapper from "./Wrapper";
 import Link from "next/link";
 import { buttonVariants } from "./Button";
+import Image from "next/image";
 import { cn } from "@/lib/utils";
 
 const Navbar = () => {
-    return (
-        <Wrapper>
-            <nav className="flex justify-between items-center mt-2.5">
-                <img src="/assets/vector.svg" alt="Logo" className="h-6" />
-                <div className="flex ">
-                    <Link href="/" className={cn(buttonVariants({ variant: "ghost" }), "px-1 md:px-2")}>Conversion</Link>
-                    <Link href="/" className={cn(buttonVariants({ variant: "ghost" }), "px-1 md:px-2")}>Resources</Link>
-                    <Link href="/" className={cn(buttonVariants({ }), "ml-3")}>Contact</Link>    
-                </div>
-            </nav>
-        </Wrapper >
-    );
-}
+  return (
+    <Wrapper>
+      <nav className="flex justify-between items-center mt-2.5">
+        <Image
+          width={200}
+          height={25}
+          src="/assets/Logo.png"
+          alt="Logo"
+          className="h-6"
+        />
+        <div className="flex ">
+          <Link
+            href="/"
+            className={cn(buttonVariants({ variant: "ghost" }), "px-1 md:px-2")}
+          >
+            Conversion
+          </Link>
+          <Link
+            href="/"
+            className={cn(buttonVariants({ variant: "ghost" }), "px-1 md:px-2")}
+          >
+            Resources
+          </Link>
+          <Link href="/" className={cn(buttonVariants({}), "ml-3")}>
+            Contact
+          </Link>
+        </div>
+      </nav>
+    </Wrapper>
+  );
+};
 
 export default Navbar;
